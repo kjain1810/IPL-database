62eb02cb0de45486ca2ff6aa68cf9c320821eba4
kjain1810 <kunaljain.2000@yahoo.in>
2020-09-23 04:38:52 +0530

    Created tables Players and Teams

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # IPL-database
 Major project for Data and Application course during Monsoon'20 semester
+
+# Loading the database
+1. ```mysql -u username -p IPL < database.sql```
+
+# Useful MySQL commands
+Adding so I dont have to google these again and again :P
+1. ```SHOW CREATE TABLE Players;``` : Shows all the details of the table
+2. ```SHOW columns FROM table_name``: Show details of columns`
+2. ```ALTER TABLE table_name ADD column_name datatype values```: Add column to table. Values can be things like ```NOT NULL```, ```PRIMARY KEY```, etc
+3. ```ALTER TABLE table_name ADD CONSTRAINT constraint_name CHECK constraint```: Add constraints
+4. ```ALTER TABLE table_name DROP CONSTRAINT constraint_name```: Remove constraint
---
+++ database.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: IPL
+-- ------------------------------------------------------
+-- Server version	8.0.21-0ubuntu0.20.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Players`
+--
+
+DROP TABLE IF EXISTS `Players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Players` (
+  `Name` varchar(80) NOT NULL,
+  `Age` int DEFAULT NULL,
+  `MoM` int DEFAULT '0',
+  `MatchesPlayed` int DEFAULT '0',
+  `PlayerID` int NOT NULL AUTO_INCREMENT,
+  `TeamID` int DEFAULT NULL,
+  PRIMARY KEY (`PlayerID`),
+  KEY `TeamID` (`TeamID`),
+  CONSTRAINT `Players_ibfk_1` FOREIGN KEY (`TeamID`) REFERENCES `Teams` (`TeamID`),
+  CONSTRAINT `Age_positive` CHECK ((`Age` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Players`
+--
+
+LOCK TABLES `Players` WRITE;
+/*!40000 ALTER TABLE `Players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Teams`
+--
+
+DROP TABLE IF EXISTS `Teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Teams` (
+  `TeamID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(80) NOT NULL,
+  `CaptainID` int DEFAULT NULL,
+  PRIMARY KEY (`TeamID`),
+  KEY `CaptainID` (`CaptainID`),
+  CONSTRAINT `Teams_ibfk_1` FOREIGN KEY (`CaptainID`) REFERENCES `Players` (`PlayerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Teams`
+--
+
+LOCK TABLES `Teams` WRITE;
+/*!40000 ALTER TABLE `Teams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Teams` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-23  4:37:56

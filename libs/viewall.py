b7c108ae1c952bd44ad715f5d4a71586c91e77e0
tejasvicsr1 <tejasvicsr1@gmail.com>
2020-10-08 17:01:21 +0530

    Fixed view

--- libs/viewall.py
+++ libs/viewall.py
@@ -1,10 +1,18 @@
+
+from tabulate import tabulate
+
+
 def viewPlayers(cur, con):
     try:
         query = "SELECT * FROM Players"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Players")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))   
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)
@@ -17,8 +25,14 @@ def viewTeams(cur, con):
         query = "SELECT * FROM Teams"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Teams")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+        # for i in range(len(res)):
+            # print(res[i])
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)
@@ -31,8 +45,14 @@ def viewMatches(cur, con):
         query = "SELECT * FROM Matches"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Matches")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))        
+        # for i in range(len(res)):
+        #     print(res[i])
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)
@@ -45,8 +65,14 @@ def viewSeasons(cur, con):
         query = "SELECT * FROM Seasons"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Seasons")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+        # for i in range(len(res)):
+        #     print(res[i])
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)
@@ -59,8 +85,14 @@ def viewStadiums(cur, con):
         query = "SELECT * FROM Stadium"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Stadiums")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+        # for i in range(len(res)):
+        # print(res[i])
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)
@@ -72,21 +104,39 @@ def viewPlayersAndTeamManagement(cur, con):
         query = "SELECT * FROM Teams"
         cur.execute(query)
         res = cur.fetchall()
-        for i in range(len(res)):
-            print(res[i])
-        teamID = int(input("Select the teamID for which you want to view the information: "))
-        query = "SELECT * FROM Players Where (TeamID = %d)" % (teamID)
-        cur.execute(query)
-        res = cur.fetchall()
-        print("Players:")
-        for i in range(len(res)):
-            print(res[i])
-        query = "SELECT * FROM TeamManagement WHERE (TeamID = %d)" % (teamID)
-        cur.execute(query)
-        res = cur.fetchall()
-        print("Team Management:")
-        for i in range(len(res)):
-            print(res[i])
+        if len(res) == 0:
+            print("There are no Teams")
+        else:
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+            # for i in range(len(res)):
+                # print(res[i])
+            teamID = int(input("Select the teamID for which you want to view the information: "))
+            query = "SELECT * FROM Players Where (TeamID = %d)" % (teamID)
+            cur.execute(query)
+            res = cur.fetchall()
+            if len(res) == 0:
+                print("There are no Players")
+            else:
+                headers = res[0].keys()
+                res = [x.values() for x in res]
+                print(tabulate(res, headers, tablefmt="pretty"))
+                # print("Players:")
+                # for i in range(len(res)):
+                    # print(res[i])
+                query = "SELECT * FROM TeamManagement WHERE (TeamID = %d)" % (teamID)
+                cur.execute(query)
+                res = cur.fetchall()
+                if len(res) == 0:
+                    print("There is no TeamMananagement")
+                else:
+                    headers = res[0].keys()
+                    res = [x.values() for x in res]
+                    print(tabulate(res, headers, tablefmt="pretty"))
+        # print("Team Management:")
+        # for i in range(len(res)):
+            # print(res[i])
     except Exception as e:
         print("Couldn't fetch list :(")
         print("Error: ", e)

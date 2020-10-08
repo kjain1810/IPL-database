000ba2538d69d5f9ea8bacc48f621676f484ae94
tejasvicsr1 <tejasvicsr1@gmail.com>
2020-10-08 16:31:37 +0530

    Fixed Search

--- libs/searchname.py
+++ libs/searchname.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
+from tabulate import tabulate
+
 def searchPlayer(cur, con):
     try:
         toSearch = input("Enter name: ")
-        query = "SELECT * FROM Players WHERE Name='%s'" % (toSearch)
+        query = "SELECT * FROM Players WHERE Name LIKE '%%%s%%'" % (toSearch)
+        # print(query)
         cur.execute(query)
         res = cur.fetchall()
         if len(res) == 0:
             print("No player with such name :(")
         else:
-            print("Here is a list of players with this name: ")
-            for i in range(len(res)):
-                print(res[i])
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+            # print("Here is a list of players with this name: ")
+            # for i in range(len(res)):
+            #     print(res[i])
         tmp = input("Press any key to continue> ")
     except Exception as e:
         print("Couldn't search :(")
@@ -22,15 +28,18 @@ def searchPlayer(cur, con):
 def searchTeam(cur, con):
     try:
         toSearch = input("Enter name: ")
-        query = "SELECT * FROM Teams WHERE Name='%s'" % (toSearch)
+        query = "SELECT * FROM Teams WHERE Name LIKE '%%%s%%'" % (toSearch)
         cur.execute(query)
         res = cur.fetchall()
         if len(res) == 0:
             print("No team with such name :(")
         else:
-            print("Here is a list of teams with this name: ")
-            for i in range(len(res)):
-                print(res[i])
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+            # print("Here is a list of teams with this name: ")
+            # for i in range(len(res)):
+            #     print(res[i])
         tmp = input("Press any key to continue> ")
     except Exception as e:
         print("Couldn't search :(")
@@ -41,15 +50,18 @@ def searchTeam(cur, con):
 def searchStadium(cur, con):
     try:
         toSearch = input("Enter name: ")
-        query = "SELECT * FROM Stadium WHERE Stadium_Name='%s'" % (toSearch)
+        query = "SELECT * FROM Stadium WHERE Stadium_Name LIKE '%%%s%%'" % (toSearch)
         cur.execute(query)
         res = cur.fetchall()
         if len(res) == 0:
             print("No stadium with such name :(")
         else:
-            print("Here is a list of stadiums with this name: ")
-            for i in range(len(res)):
-                print(res[i])
+            headers = res[0].keys()
+            res = [x.values() for x in res]
+            print(tabulate(res, headers, tablefmt="pretty"))
+            # print("Here is a list of stadiums with this name: ")
+            # for i in range(len(res)):
+            #     print(res[i])
         tmp = input("Press any key to continue> ")
     except Exception as e:
         print("Couldn't search :(")
